6d909576dab19ac21b69707744f6257a1aa3d290
Abdulmohsen Alabra <abdulalabra123@gmaile.com>
2018-10-18 17:35:33 -0700

    sadkjh

--- controllers/articlesAPI.js
+++ controllers/articlesAPI.js
@@ -1,15 +1,17 @@
 var axios = require("axios");
 var cheerio = require("cheerio");
 var Article = require("../models/article");
+var express = require('express');
+var app = express();
 
 module.exports = function (app) {
-    app.get("/", function (req, res) {
+    app.get("/all", function (req, res) {
         Article.find({}).then(function (data) {
-            res.render("news", {data});
+            res.render("newscrape", { data });
         });
     });
 
-    app.get("/news", function (req, res) {
+    app.get("/", function (req, res) {
         axios.get("https://news.google.com/?hl=en-US&gl=US&ceid=US:en").then(function (response) {
             var $ = cheerio.load(response.data);
             var data = [];
@@ -24,16 +26,24 @@ module.exports = function (app) {
                 var image = $(this).parent().parent().parent().parent().children("figure").children("img").attr("src");
                 var summary = $(this).parent().children("p").text();
 
-                result.title = title;
-                result.summary = summary;
-                result.link = link;
-                result.image = image;
-                data.push(result);
-                Article.create(result).then(function (dataAdded) {
-                    console.log(dataAdded); 
-                });
-            });    
-            res.render("news", {data});
+                if (image === undefined) {
+                    console.log(image);
+                }
+                else {
+                    result.title = title;
+                    result.summary = summary;
+                    result.link = link;
+                    result.image = image;
+                    data.push(result);
+                    Article.create(result).then(function (dataAdded) {
+                    }).catch(function(err){
+                        console.log("helloooo");
+                        console.log(err);
+                        
+                    });
+                }
+            });
+            res.render("news", { data });
         });
     });
 }
--- server.js
+++ server.js
@@ -5,16 +5,18 @@ var app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+
 
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/scraping", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraping";
 
+mongoose.connect(MONGODB_URI);
 //paste here if it does not work
-var routes = require("./controllers/articlesAPI")(app);
+require("./controllers/articlesAPI")(app);
+app.use(express.static("public"));
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
 });
